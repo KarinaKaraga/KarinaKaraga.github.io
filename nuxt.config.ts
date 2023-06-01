724a59e01cedd981304887512f56aa302229864e
Petko Petkov <petko.petkov@next-stream.com>
2023-06-02 01:20:47 +0300

    fixes gh actions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/nuxt-github-pages/' // baseURL: '/<repository>/'
+  },
   nitro: {
     output: {
       publicDir: './dist', // This setting can avoid that the packed dist file is a soft connection
