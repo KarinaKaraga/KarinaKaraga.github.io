195b5430e7f262546015644199d303261c7a9583
Petko Petkov <petko.petkov@next-stream.com>
2023-06-02 01:26:13 +0300

    final fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/nuxt-github-pages/' // baseURL: '/<repository>/'
-  },
   css: ['~/assets/css/main.css'],
   postcss: {
       plugins: {
