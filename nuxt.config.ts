905add955dd1a0279656ceb556c6120365e6139f
Petko Petkov <petko.petkov@next-stream.com>
2023-06-02 00:40:21 +0300

    fixes CI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ['~/assets/css/main.css'],
-    postcss: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
+  nitro: {
+    output: {
+      publicDir: './dist', // This setting can avoid that the packed dist file is a soft connection
     },
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+  },
 })
